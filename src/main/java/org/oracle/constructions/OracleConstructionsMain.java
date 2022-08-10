6dcc436f1e882e1d732f6df39dbc211b4da5c419
mukesh <mukeshmande@gmail.com>
2022-08-11 00:14:03 +0530

    input from console & average build time

--- src/main/java/org/oracle/constructions/OracleConstructionsMain.java
+++ src/main/java/org/oracle/constructions/OracleConstructionsMain.java
@@ -3,14 +3,8 @@ package org.oracle.constructions;
 
 import org.oracle.constructions.beans.CustomerDetailsBean;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class OracleConstructionsMain {
 
@@ -22,11 +16,25 @@ public class OracleConstructionsMain {
 
     public static void main(String[] args) throws IOException {
 
-        Reader inputString = new StringReader(data);
+                Reader inputString = new StringReader(data);
         BufferedReader reader = new BufferedReader(inputString);
         String line;
+
+        String lineText;
+        List<String> inputLines = new ArrayList<>();
+
+        System.out.println("Press Enter key to add next input on next line & press Enter key two times to submit enter");
+        System.out.println("Enter Input from here :- ");
+
+//        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+//        while ((lineText = stdin.readLine()) != null && lineText.length()!= 0) {
+//            inputLines.add(lineText);
+//        }
+
+
         List<CustomerDetailsBean> customerDetailsList = new ArrayList<>();
         while ((line = reader.readLine()) != null) {
+//        for(String line : inputLines){
             String lineData[] = line.split(",");
             if (lineData.length == 6) {
                 CustomerDetailsBean customerDetailsBean = new CustomerDetailsBean();
@@ -38,6 +46,7 @@ public class OracleConstructionsMain {
                 customerDetailsBean.setBuildduration(lineData[5]);
                 customerDetailsList.add(customerDetailsBean);
             }
+
         }
 
         generateReport(customerDetailsList);
@@ -69,20 +78,29 @@ public class OracleConstructionsMain {
         uniqueCustomerIdForContractId.forEach((k,v) ->{
             System.out.println(k+"->"+v.size());
         });
-
+        System.out.println();
 
         System.out.println("The Number of Unique customer Id for each geo zone");
         uniqueCustomerIdForGeozone.forEach((k,v) ->{
             System.out.println(k+"->"+v.size());
         });
+        System.out.println();
 
+
+        HashMap<String, Long> averageBuidDuration = new HashMap<>();
+        avgBuildDurationForGeozone.forEach((k,v) ->{
+            long totalBuildTime;
+            totalBuildTime = v.stream().mapToLong(time -> Integer.parseInt(time.substring(0, time.lastIndexOf('s')))).sum();
+            averageBuidDuration.put(k, totalBuildTime/v.size());
+        });
 
         System.out.println("The average build duration for each geo zone");
-        avgBuildDurationForGeozone.forEach((k,v) ->{
+        averageBuidDuration.forEach((k,v) -> {
             System.out.println(k+"->"+v);
         });
 
 
+        System.out.println();
         System.out.println("The list of Unique customer Id for each geo zone");
         uniqueCustomerIdForGeozone.forEach((k,v) ->{
             System.out.println(k+"->"+v);
